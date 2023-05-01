6776390edd9f63b9b9d9fd2f9c82c81708f11423
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-02 00:12:09 +0300

    Update task name on submit

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const TodoInput = ({ addTodoItem }) => {
-	const [title,setTitle] =useState(' ')
+	const [title,setTitle] =useState(' ');
+	const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
 	const handleSubmit = (e)=>{
 		e.preventDefault();
-		addTodoItem(title)
+		addTodoItem(title);
     setTitle('');
 	}
 	return(
@@ -13,7 +16,9 @@ const TodoInput = ({ addTodoItem }) => {
       <input
         type="text"
 				name="todo-task"
-				placeholder='Add todo...'/>
+				placeholder='Add todo...'
+				value={title}
+        onChange={handleChange}/>
 			<button type="submit">Enter</button>
 		</form>
 	)
