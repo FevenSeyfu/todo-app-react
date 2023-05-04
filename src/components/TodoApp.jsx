e90dd42544bd1eecb6433cc596cbbdedec0af71d
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-04 18:29:46 +0300

    Use useEffect hook to update Title

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,14 +1,18 @@
 import TodoHeader from './TodoHeader';
 import TodoLogic from './TodoLogic';
 import '../styles/app.css';
+import Navbar from './NavBar';
 
 const TodoApp = () => {
 	return(
 		<div className="wrapper">
+      <header>
+        <Navbar />
+			</header>
       <div className="todos">
 				<TodoHeader />
 				<TodoLogic />
-		</div>
+      </div>
 		</div>
 	)
 }
